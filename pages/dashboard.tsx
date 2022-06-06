7f4f4b34358fda926ed0a88541bb58f6e095e2c1
SeifMohy <seifmohy@gmail.com>
2022-06-06 19:01:25 +0200

    minor amendments

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -83,9 +83,9 @@ const Dashboard = () => {
     enableReinitialize: true,
     onSubmit: async (values: any, resetForm: any) => {
       console.log(values);
-      // const res = axios.put('/api/userLogs', values);
-      // const data = await res;
-      // console.log('userLogs', data); //TODO: Reset Form
+      const res = axios.put('/api/userLogs', values);
+      const data = await res;
+      console.log('userLogs', data); //TODO: Reset Form
     }
   });
   console.log(todaysWorkout);
