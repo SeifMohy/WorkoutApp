125c5ec629a8fa86f98a88d673aaa34d6309ea78
SeifMohy <seifmohy@gmail.com>
2022-06-08 18:26:18 +0200

    mapping

--- pages/calendarTest.tsx
+++ pages/calendarTest.tsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 import useSWR from 'swr';
 import { WorkoutHistoryCard } from 'types';
 import moment from 'moment';
+import exercise from './api/exercise';
 
 const fetchWorkoutHistory = (url: string) =>
   axios.get(url).then((res) => res.data);
@@ -30,46 +31,48 @@ const calendar = () => {
         </div>
         <div>
           <>
-            {data.workouts.map((x, idx:number) => {
+            {data.workouts.map((workout, idx: number) => {
               return (
                 <div className="grid sm:grid-cols-1 lg:grid-cols-2 py-8 md:px-8 px-0 bg-white rounded-md">
                   <div>
                     <h1>Workout</h1>
                     <span className="block text-xs text-gray-400">
-                      {
-                        x.workoutName
-                      }{' '}
-                      Day
+                      {workout.workoutName} Day
                     </span>
                   </div>
                   <div>
                     <h1>Date</h1>
                     <span className="block text-xs text-gray-400">
-                      {moment(data.workoutDates[idx]).format('MMM Do YY')}
+                      {moment(workout.date).format('MMM Do YY')}
                     </span>
                   </div>
                   <div className="bg-white my-8 flex rounded-l-3xl">
-
                     <>
-                    {x.exercises.map((x)=>x[1].name)}
-                    </>
-{/*
-                  <img
+                      {workout.workoutLines.map((exercise) => {
+                        return (
+                          <div>
+                            <img
                               className="rounded-l-3xl"
-                              src="/images/signin.jpg"
+                              src={`${exercise.exercise.imageUrl}`}
                               alt="Picture of the author"
                               width={150}
                               height={200}
                             />
-                     <>
-                      {x[1].map((x) => {
-                        return (
-                            <span className="block text-xs text-gray-400">
-                            {x.reps} reps . {x.weight}kg
-                          </span>
+                            <div>
+                              <>
+                                {exercise.logs.map((logs) => {
+                                  return (
+                                    <span className="block text-xs text-gray-400">
+                                      {logs.reps} reps . {logs.weight}kg
+                                    </span>
+                                  );
+                                })}
+                              </>
+                            </div>
+                          </div>
                         );
                       })}
-                    </> */}
+                    </>
                   </div>
                 </div>
               );
